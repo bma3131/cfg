4ca0352826ce48dc6830290ff595a989343f0ce4
Mikhail <bma3131@gmail.com>
2021-03-13 20:14:43 +0300

    m

--- emacs/init.el
+++ emacs/init.el
@@ -5,20 +5,23 @@
 (tool-bar-mode -1)
 ;(add-to-list 'default-frame-alist '(fullscreen . maximized))
 (global-linum-mode 1)
+(setq inhibit-startup-screen t)
+(setq column-number-mode t)
+(auto-fill-mode -1)
+
+(setq-default fill-column 80)
+(global-display-fill-column-indicator-mode 1)
+
 (add-to-list 'default-frame-alist '(font . "Consolas" ))
 (set-face-attribute 'default t :font "Consolas" )
 (set-face-attribute 'default nil :height 100)
 (set-fontset-font "fontset-default" 'cyrillic "DejaVu Sans Mono 11")
 (set-fontset-font "fontset-default" 'greek "DejaVu Sans Mono 11")
-(setq inhibit-startup-screen t)
+
 ;(setq backup-directory-alist '(("." . "C:\\Users\\bma\\.emacs-backups")))
 ;(setq backup-by-copying t)
 (setq auto-save-default nil)
 (setq make-backup-files nil)
-(setq-default fill-column 80)
-(global-display-fill-column-indicator-mode 1)
-(setq column-number-mode t)
-(auto-fill-mode -1)
 
 (setq explicit-shell-file-name "c:\\windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe")
 (setq explicit-powershell.exe-args '("-Command" "-" ))
